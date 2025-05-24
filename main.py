60faad2ebb38b0387043fb20c3ebe1ca7328c076
Ruslan Acharaev <racharaev@gmail.com>
2025-05-24 14:32:16 +0300

    Edit arguments description

--- main.py
+++ main.py
@@ -4,20 +4,49 @@ from core import process_args, REPORTS, OUTPUTS
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Генерирует отчеты по данным сотрудников')
-    parser.add_argument('files', metavar='FILE', type=str, nargs='+',
-                        help='CSV файлы или пути к CSV файлам')
-    parser.add_argument('--report', type=str, required=True,
-                        choices=REPORTS.keys(),
-                        help='Тип отчета для генерации')
-    parser.add_argument('--format', type=str, default='json',
-                        choices=OUTPUTS.keys(),
-                        help='Формат файла для отчета')
-    parser.add_argument('-n', type=str, default='report',
-                        help='Название файла отчета')
-    parser.add_argument('-c', action="store_false", default=True,
-                        help="Отключить вывод в консоль (по умолчанию: вывод включён)"
-                        )
+    parser = argparse.ArgumentParser(
+        description='Генератор отчетов по заработной плате сотрудников. '
+                    'Позволяет создавать различные отчеты на основе CSV-файлов с данными.'
+    )
+    parser.add_argument(
+        'files',
+        metavar='FILE',
+        type=str,
+        nargs='+',
+        help='Один или несколько CSV-файлов с данными сотрудников. '
+             'Можно указать через пробел несколько файлов или пути к ним.'
+    )
+    parser.add_argument(
+        '--report',
+        type=str,
+        required=True,
+        choices=REPORTS.keys(),
+        help='Тип генерируемого отчета. Доступные варианты: '
+             f'{", ".join(REPORTS.keys())}.'
+    )
+    parser.add_argument(
+        '--format',
+        type=str,
+        default='json',
+        choices=OUTPUTS.keys(),
+        help='Формат выходного файла отчета. По умолчанию: json. '
+             f'Доступные форматы: {", ".join(OUTPUTS.keys())}.'
+    )
+    parser.add_argument(
+        '-n', '--name',
+        type=str,
+        default='report',
+        help='Базовое название для выходного файла (без расширения). '
+             'По умолчанию: "report".'
+    )
+    parser.add_argument(
+        '-c', '--console',
+        action="store_false",
+        default=True,
+        help='Отключает вывод отчета в консоль. '
+             'По умолчанию отчет выводится и в файл, и в консоль. '
+             'Если указан этот флаг, вывод будет только в файл.'
+    )
 
     process_args(parser.parse_args())
 
