083816e447f9d9213c756bec9b7cc0fad52ef454
Ruslan Acharaev <racharaev@gmail.com>
2025-05-24 13:31:24 +0300

    Add main process

---
+++ core/process_args.py
@@ -0,0 +1,15 @@
+from core import REPORTS, OUTPUTS
+from data_parser import parse_csv_files, normalize_data
+from error_checkers import check_files, check_args
+
+
+def process_args(args):
+    if check_args(args) and check_files(args.files):
+        raw_data = parse_csv_files(args.files)
+        normalized_data = normalize_data(raw_data)
+        report_generator = REPORTS.get(args.report)
+        report = report_generator(normalized_data, args.c)
+        formatter = OUTPUTS.get(args.format)
+        formatter(report, args.n)
+        return 0
+    return 1
